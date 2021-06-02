6c5d008c4cab795935148539e7667391ae23bd2d
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-03 02:28:11 +0530

    Create a Route object to get through the index function of HomeController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+//use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/', 'HomeController@index');
 
 Route::get('/about', function () {
     echo 'Hello';
