fb18b3e88f778c4e0d9f16833b493a77dc53503e
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-03 04:58:24 +0530

    Create a form to submit messages

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,17 @@
 @section('title', 'HomePage')
 
 @section('content')
+
+Post a message:
+
+<form action="/create" method="post">
+    <input type="text" id="title" placeholder="Title">
+    <input type="text" id="content" placeholder="Content">
+    <button type="submit">Submit</button>
+</form>
+
 Recent messages:
+
 <ul>
     @foreach ($messages as $message)
     <li>{{ $message->title }} - {{ $message->content }} - {{ $message->created_at }}</li>
