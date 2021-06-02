76c67968be4557081d5174c08ca3a5ca9bbb5ce9
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-03 04:03:51 +0530

    Migrate

--- database/migrations/2021_06_02_212111_create_messages_table.php
+++ database/migrations/2021_06_02_212111_create_messages_table.php
@@ -14,7 +14,7 @@ class CreateMessagesTable extends Migration
     public function up()
     {
         Schema::create('messages', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('title');
             $table->string('content');
             $table->timestamps();
